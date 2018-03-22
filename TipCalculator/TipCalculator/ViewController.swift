40c98c2d7617c0a542f7c91bed1594234554fc31
William Yang <williamyang93@gmail.com>
2018-03-21 18:43:01 -0700

    Return to this commit to work on adding folding table view

--- TipCalculator/TipCalculator/ViewController.swift
+++ TipCalculator/TipCalculator/ViewController.swift
@@ -67,85 +67,89 @@ class ViewController: UIViewController, UITextFieldDelegate {
         updateLabel()
         //print("Trying to update label")
     }
-    // Custom Cell Functions
-    var cellDescriptors: NSMutableArray!
-    @IBOutlet weak var tblExpandable: UITableView!
-    var visibleRowsPerSection = [[Int]]()
-    func loadCellDescriptors() {
-        if let path = Bundle.main.path(forResource: "CellDescriptor", ofType: "plist") {
-            cellDescriptors = NSMutableArray(contentsOfFile: path)
-            getIndicesOfVisibleRows()
-            tblExpandable.reloadData()
-        }
-    }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        configureTableView()
-        loadCellDescriptors()
-        getIndicesOfVisibleRows()
-    }
-    
-    func configureTableView(){
-        
-    }
-    
-    func getIndicesOfVisibleRows() {
-        visibleRowsPerSection.removeAll()
-        for currentSectionCells in cellDescriptors {
-            var visibleRows = [Int]()
-            if let currentSection = currentSectionCells as? [[String:AnyObject]] {
-                for row in 0...(currentSection.count - 1){
-                    if currentSection[row]["isVisible"] as! Bool == true {
-                        visibleRows.append(row)
-                        
-                    }
-                }
-            } else {
-            print("Didnt let")
-            }
-            visibleRowsPerSection.append(visibleRows)
-        }
-    }
-    
-    func getCellDescriptorForIndexPath(indexPath: NSIndexPath) -> [String: AnyObject] {
-        let indexOfVisibleRow = visibleRowsPerSection[indexPath.section][indexPath.row]
-        let cellDescriptor = cellDescriptors[indexPath.section][indexOfVisibleRow] as! [String:AnyObject]
-        return cellDescriptor
-    }
-    
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        if cellDescriptors != nil {
-            return cellDescriptors.count
-        }
-        else {
-            return 0
-        }
-    }
-    
-    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        switch section {
-        case 0:
-            return "Personal"
-        case 1:
-            return "Prefernece"
-        default:
-            return "Work"
-        }
-    }
-    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        let currentCellDescriptor = getCellDescriptorForIndexPath(indexPath)
-        
-        switch currentCellDescriptor["cellIdentifier"] as! String {
-        case "idCellNormal":
-            return 60.0
-            
-        case "idCellDatePicker":
-            return 270.0
-            
-        default:
-            return 44.0
-        }
-    }
+    /*
+//    // Custom Cell Functions
+//    var cellDescriptors: NSMutableArray!
+//    @IBOutlet weak var tblExpandable: UITableView!
+//    var visibleRowsPerSection = [[Int]]()
+//    func loadCellDescriptors() {
+//        if let path = Bundle.main.path(forResource: "CellDescriptor", ofType: "plist") {
+//            cellDescriptors = NSMutableArray(contentsOfFile: path)
+//            getIndicesOfVisibleRows()
+//            tblExpandable.reloadData()
+//        }
+//    }
+//    
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//        configureTableView()
+//        loadCellDescriptors()
+//        getIndicesOfVisibleRows()
+//    }
+//    
+//    func configureTableView(){
+//        
+//    }
+//    
+//    func getIndicesOfVisibleRows() {
+//        visibleRowsPerSection.removeAll()
+//        for currentSectionCells in cellDescriptors {
+//            var visibleRows = [Int]()
+//            if let currentSection = currentSectionCells as? [[String:AnyObject]] {
+//                for row in 0...(currentSection.count - 1){
+//                    if currentSection[row]["isVisible"] as! Bool == true {
+//                        visibleRows.append(row)
+//                        
+//                    }
+//                }
+//            } else {
+//            print("Didnt let")
+//            }
+//            visibleRowsPerSection.append(visibleRows)
+//        }
+//    }
+//    
+//    func getCellDescriptorForIndexPath(indexPath: NSIndexPath) -> [String: AnyObject] {
+//        let indexOfVisibleRow = visibleRowsPerSection[indexPath.section][indexPath.row]
+//        let cellDescriptor = cellDescriptors[indexPath.section][indexOfVisibleRow] as! [String:AnyObject]
+//        return cellDescriptor
+//    }
+//    
+//    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+//        if cellDescriptors != nil {
+//            return cellDescriptors.count
+//        }
+//        else {
+//            return 0
+//        }
+//    }
+//    
+//    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        switch section {
+//        case 0:
+//            return "Personal"
+//        case 1:
+//            return "Prefernece"
+//        default:
+//            return "Work"
+//        }
+//    }
+//    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+//        let currentCellDescriptor = getCellDescriptorForIndexPath(indexPath)
+//        
+//        switch currentCellDescriptor["cellIdentifier"] as! String {
+//        case "idCellNormal":
+//            return 60.0
+//            
+//        case "idCellDatePicker":
+//            return 270.0
+//            
+//        default:
+//            return 44.0
+//        }
+//    }
+}
+ */
 }
 
