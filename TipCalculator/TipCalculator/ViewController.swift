d0da4de615398d77740289f8c4b781d42d5d8d23
William Yang <williamyang93@gmail.com>
2018-03-22 21:33:54 -0700

    New icon, added in 10, 18% choices, added in validation message

--- TipCalculator/TipCalculator/ViewController.swift
+++ TipCalculator/TipCalculator/ViewController.swift
@@ -11,37 +11,42 @@ import os.log
 
 class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var initialTotal: UITextField!
+    
+    @IBOutlet weak var postTipAmount10: UILabel!
+    @IBOutlet weak var tip10: UILabel!
+    @IBOutlet weak var tipAmount10: UILabel!
+    
     @IBOutlet weak var postTipAmount15: UILabel!
     @IBOutlet weak var tip15: UILabel!
     @IBOutlet weak var tipAmount15: UILabel!
     
+    @IBOutlet weak var tip18: UILabel!
+    @IBOutlet weak var tipAmount18: UILabel!
+    @IBOutlet weak var postTipAmount18: UILabel!
+    
+    @IBOutlet weak var errorLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         initialTotal.delegate = self
+        self.addDoneButtonOnKeyboard()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
-    func updateLabel() {
-        if let totalPrice = initialTotal.text, let doubleTotalPrice = Double(totalPrice), let tipPercentage = tip15.text, let doubleTipPercentage = Double(tipPercentage) {
+    func updateLabel(tip: UILabel, tipAmount: UILabel, postTipAmount: UILabel) {
+        if let totalPrice = initialTotal.text, let doubleTotalPrice = Double(totalPrice), let tipPercentage = tip.text, let doubleTipPercentage = Double(tipPercentage) {
           
-            postTipAmount15.text = String(format: "%.2f", calculateTotalTip(initialTotal: doubleTotalPrice, percent: doubleTipPercentage))
-            tipAmount15.text = String(format: "%.2f", calculateTipAmount(initialTotal: doubleTotalPrice, percent: doubleTipPercentage))
+            postTipAmount.text = String(format: "%.2f", calculateTotalTip(initialTotal: doubleTotalPrice, percent: doubleTipPercentage))
+            tipAmount.text = String(format: "%.2f", calculateTipAmount(initialTotal: doubleTotalPrice, percent: doubleTipPercentage))
             print(String(format: "%.2f", calculateTotalTip(initialTotal: doubleTotalPrice, percent: doubleTipPercentage)))
             print("Tip: " + tipAmount15.text!)
         } else {
-            let initialTotalPrice = initialTotal.text!
-            let doubleTotalPrice = Double(initialTotalPrice)
-            let tipPercentage = tip15.text!
-            let doubleTipPercentage = Double(tipPercentage)
-            print(doubleTotalPrice)
-            print(doubleTipPercentage)
-            
-            print("Not a proper number: " + initialTotal.text!)
+            errorLabel.text = "Not a valid number!"
         }
         
     }
@@ -59,15 +64,43 @@ class ViewController: UIViewController, UITextFieldDelegate {
         return true
     }
     func textFieldDidBeginEditing(_ textField: UITextField) {
-        
-        print("Began editing, trying to update label")
+        errorLabel.text = ""
     }
     func textFieldDidEndEditing(_ textField: UITextField) {
         initialTotal.text = textField.text
-        updateLabel()
-        //print("Trying to update label")
+        updateLabel(tip: tip10, tipAmount: tipAmount10, postTipAmount: postTipAmount10)
+        updateLabel(tip: tip15, tipAmount: tipAmount15, postTipAmount: postTipAmount15)
+        updateLabel(tip: tip18, tipAmount: tipAmount18, postTipAmount: postTipAmount18)
+        
+    }
+    //Key board functions
+    func addDoneButtonOnKeyboard() {
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 320, height: 50))
+        let clearToolbar: UIToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 320, height: 50))
+        clearToolbar.barStyle      = UIBarStyle.default
+        doneToolbar.barStyle       = UIBarStyle.default
+        let flexSpace              = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil)
+        let clear: UIBarButtonItem = UIBarButtonItem(title: "Clear", style: UIBarButtonItemStyle.done, target: self, action: #selector(ViewController.clearButtonAction))
+        let done: UIBarButtonItem  = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.done, target: self, action: #selector(ViewController.doneButtonAction))
+        
+        var items = [UIBarButtonItem]()
+        items.append(flexSpace)
+        items.append(clear)
+        items.append(done)
+        
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+        
+        self.initialTotal.inputAccessoryView = doneToolbar
     }
     
+    @objc func doneButtonAction() {
+        self.initialTotal.resignFirstResponder()
+    }
+    
+    @objc func clearButtonAction() {
+        initialTotal.text = ""
+    }
     /*
 //    // Custom Cell Functions
 //    var cellDescriptors: NSMutableArray!
